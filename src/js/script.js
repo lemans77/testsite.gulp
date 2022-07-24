9e57b42fdaedbef29a0f3bb6116b88c9344f5718
lemans77 <oleg.boiechko@gmail.com>
2022-07-24 17:51:44 +0300

    Add script active navbar and nav hide

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 $('.block').html('<h2>Baked fresh daily by bakers with passion.</h2>');
+
+// active navbar
+let nav = document.querySelector('.navigation-wrap');
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add('scroll-on');
+  } else {
+    nav.classList.remove('scroll-on');
+  }
+};
+
+// nav hide
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
+navLinks.forEach((l) => {
+  l.addEventListener('click', () => { bsCollapse.toggle() })
+})
