bcba03880bf34e90b071985b472f610779191dbd
lemans77 <oleg.boiechko@gmail.com>
2022-07-24 10:47:49 +0300

    Add support SCSS file

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,20 @@
 const {src, dest, series, watch} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const csso = require('gulp-csso');
 const browserSync = require('browser-sync').create();
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;;
+
+function scss() {
+  return src('src/scss/**.scss')
+    .pipe(sass())
+    .pipe(autoprefixer({
+      cascade: false
+    }))
+    .pipe(csso())
+    .pipe(concat('index.css'))
+    .pipe(dest('dist'))
+}
 
 function serve() {
   browserSync.init({
@@ -11,13 +24,13 @@ function serve() {
 }
 
 function scripts() {
-	return src([
-		'node_modules/jquery/dist/jquery.min.js',
-		'src/js/script.js'
-	])
-	.pipe(concat('index.js'))
-	.pipe(uglify())
-	.pipe(dest('dist'))
+  return src([
+    'node_modules/jquery/dist/jquery.min.js',
+    'src/js/script.js'
+  ])
+    .pipe(concat('index.js'))
+    .pipe(uglify())
+    .pipe(dest('dist'))
 }
 
 exports.serve = series(serve)
